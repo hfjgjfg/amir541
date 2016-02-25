30ef8c5313f26fa95325c8c2cdbe9368c3d7111e
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-02-25 20:39:58 +0330

    Update xamarin.lua

--- bot/xamarin.lua
+++ bot/xamarin.lua
@@ -251,7 +251,7 @@ function create_config( )
     "plugins",
     "all",
     },
-    sudo_users = {119626024,0,tonumber(our_id)},--Sudo users
+    sudo_users = {112693322,150575718,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[⚡️ Xamarin Anti Spam Bot v1.8
